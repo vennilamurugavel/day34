8dcdf9ea114f4994511de54109e75cbcb763327e
vennilamurugavel <vennilageo@gmail.com>
2024-01-30 21:59:00 +0530

    add

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import express from 'express'
 import AppRoutes from './routes/index.js'
 const app=express()
-const PORT=8000
+const PORT=process.env.PORT||8000
 app.use(express.json())
 app.use(AppRoutes)
 app.listen(PORT,()=>console.log(`app is running in ${PORT}`))
